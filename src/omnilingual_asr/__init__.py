8f07c6045fc1b719e060b71ba103b4214b8dd5ec
Artyom Kozhevnikov <artyomko@fb.com>
2025-12-15 02:51:54 -0800

    chaning the version for the release

--- src/omnilingual_asr/__init__.py
+++ src/omnilingual_asr/__init__.py
@@ -26,7 +26,7 @@ from omnilingual_asr.models.wav2vec2_ssl.config import (
     register_omnilingual_asr_wav2vec2_ssl_configs,
 )
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 def setup_fairseq2_extension(container: DependencyContainer) -> None:
